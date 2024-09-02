7c2daada5c131380ea1ffda3b5c103804be9f903
jackfauji <jackfauji1000@gmail.com>
2024-09-02 09:38:48 +0530

    Add files via upload

---
+++ l.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+	int data;
+	struct node *next;
+}sn;
+
+int main()
+{
+	sn *head,*newnode=0,*temp=0;
+	head=0;
+	int ch=1;
+	while(ch)
+	{
+		newnode=(sn*)malloc(sizeof(sn));
+		printf("enter data to store");
+		scanf("%d",&newnode->data);
+		newnode->next=0;
+		if(head==0)
+		{
+			head=newnode;
+			temp=newnode;
+		}
+		else{
+			temp->next=newnode;
+			temp=newnode;
+		}
+		printf("do you want to (0,1)?");
+		scanf("%d",&ch);
+	}
+	temp=head;
+	while(temp!=0)
+	{
+		printf("%d ",temp->data);
+		temp=temp->next;
+	}
+}
